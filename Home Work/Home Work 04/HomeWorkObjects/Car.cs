f34b3dfbc50dd9d27bb77226ef27e7cc4ce7cb91
Martin Kenov <martin.kenov@gmail.com>
2018-10-30 00:04:41 +0200

    Classes and Objects Home Work

--- Home Work/Home Work 04/HomeWorkObjects/Car.cs
+++ Home Work/Home Work 04/HomeWorkObjects/Car.cs
@@ -1,21 +1,21 @@
 ﻿namespace HomeWorkObjects
 {
-    class Car
+    public class Car
     {
-        private string carBrand;
-        private string carModel;
-        private double carMileAge;
-        private double carPrice;
+        private string _carBrand;
+        private string _carModel;
+        private double _carMileAge;
+        private double _carPrice;
 
         public string CarBrand
         {
             get
             {
-                return this.carBrand;
+                return this._carBrand;
             }
             set
             {
-                this.carBrand = value;
+                this._carBrand = value;
             }
         }
 
@@ -23,76 +23,63 @@
         {
             get
             {
-                return this.carModel;
+                return this._carModel;
             }
             set
             {
-                this.carModel = value;
+                this._carModel = value;
             }
         }
-
-        //Why this behavior is Not Possible ???
-        //public double MileAge(string input)
-        //{
-        //    get
-        //    {
-        //        return double.TryParse(input, out mileAge);
-        //    }
-        //    set
-        //    {
-        //        this.mileAge = value;
-        //    }
-        //}
 
         public double CarMileage
         {
             get
             {
-                return this.carMileAge;
+                return this._carMileAge;
             }
             set
             {
-                this.carMileAge = value;
+                this._carMileAge = value;
             }
         }
 
-        public double CarPrice { get => carPrice; set => carPrice = value; }
+        public double CarPrice { get => _carPrice; set => _carPrice = value; }
 
         public Car()
         {
-            this.carBrand = null;
-            this.carModel = null;
-            this.carMileAge = 0;
-            this.carPrice = 0;
+            this._carBrand = null;
+            this._carModel = null;
+            this._carMileAge = 0;
+            this._carPrice = 0;
         }
 
         public Car(string carBrand) : this()
         {
-            this.carBrand = carBrand;
+            this._carBrand = carBrand;
             // How to add Car() to initialize other properties???
         }
 
         public Car(string carBrand, string carModel) : this(carBrand)
         {
             // How to use Car() to initialize already initialized properties???
-            this.carModel = carModel;
-
+            //this.carBrand = carBrand;
+            this._carModel = carModel;
         }
 
-        public Car(string carBrand, string carModel, double carMileAge)
+        public Car(string carBrand, string carModel, double carMileAge) : this (carBrand, carModel)
         {
-            this.carBrand = carBrand;
-            this.carModel = carModel;
             // Can I add verification for double here: double.TryPrace ???
-            this.carMileAge = carMileAge;
+            //this.carBrand = carBrand;
+            //this.carModel = carModel;
+            this._carMileAge = carMileAge;
         }
 
-        public Car(string carBrand, string carModel, double carMileAge, double carPrice)
+        public Car(string carBrand, string carModel, double carMileAge, double carPrice) : this (carBrand, carModel, carMileAge)
         {
-            this.carBrand = carBrand;
-            this.carModel = carModel;
-            this.carMileAge = carMileAge;
-            this.carPrice = carPrice;
+            //this.carBrand = carBrand;
+            //this.carModel = carModel;
+            //this.carMileAge = carMileAge;
+            this._carPrice = carPrice;
         }
 
 
