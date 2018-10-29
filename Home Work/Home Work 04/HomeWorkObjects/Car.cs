7222ffe6c9a0827eac8d5f79b9df20904bc61aa1
Martin Kenov <martin.kenov@gmail.com>
2018-10-29 07:52:19 +0200

    Home Work Objects - Task #1 - Car Class Added

---
+++ Home Work/Home Work 04/HomeWorkObjects/Car.cs
@@ -0,0 +1,101 @@
+﻿namespace HomeWorkObjects
+{
+    class Car
+    {
+        private string carBrand;
+        private string carModel;
+        private double carMileAge;
+        private double carPrice;
+
+        public string CarBrand
+        {
+            get
+            {
+                return this.carBrand;
+            }
+            set
+            {
+                this.carBrand = value;
+            }
+        }
+
+        public string CarModel
+        {
+            get
+            {
+                return this.carModel;
+            }
+            set
+            {
+                this.carModel = value;
+            }
+        }
+
+        //Why this behavior is Not Possible ???
+        //public double MileAge(string input)
+        //{
+        //    get
+        //    {
+        //        return double.TryParse(input, out mileAge);
+        //    }
+        //    set
+        //    {
+        //        this.mileAge = value;
+        //    }
+        //}
+
+        public double CarMileage
+        {
+            get
+            {
+                return this.carMileAge;
+            }
+            set
+            {
+                this.carMileAge = value;
+            }
+        }
+
+        public double CarPrice { get => carPrice; set => carPrice = value; }
+
+        public Car()
+        {
+            this.carBrand = null;
+            this.carModel = null;
+            this.carMileAge = 0;
+            this.carPrice = 0;
+        }
+
+        public Car(string carBrand)
+        {
+            this.carBrand = carBrand;
+            // How to add Car() to initialize other properties???
+        }
+
+        public Car(string carBrand, string carModel)
+        {
+            // How to use Car() to initialize already initialized properties???
+            this.carBrand = carBrand;
+            this.carModel = carModel;
+
+        }
+
+        public Car(string carBrand, string carModel, double carMileAge)
+        {
+            this.carBrand = carBrand;
+            this.carModel = carModel;
+            // Can I add verification for double here: double.TryPrace ???
+            this.carMileAge = carMileAge;
+        }
+
+        public Car(string carBrand, string carModel, double carMileAge, double carPrice)
+        {
+            this.carBrand = carBrand;
+            this.carModel = carModel;
+            this.carMileAge = carMileAge;
+            this.carPrice = carPrice;
+        }
+
+
+    }
+}
---
+++ Home Work/Home Work 04/HomeWorkObjects/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HomeWorkObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Console.Write("Enter Cars Number: ");
+            input = Console.ReadLine();
+
+            int newCarsNumber = 0;
+            Int32.TryParse(input, out newCarsNumber);
+
+            Car[] cars = new Car[newCarsNumber];
+
+            Console.Clear();
+
+            Console.WriteLine(" --- Car #{i} Description --- ");
+            Console.Write("Enter Car #{i} Brand: ");
+            input = Console.ReadLine();
+            //cars[0].CarBrand = input;
+            //cars[0].CarBrand = Car[0](input);
+
+            Console.Write("Enter Car #{i} Model: ");
+            cars[0].CarModel = Console.ReadLine();
+
+            bool IsCarMileageValid = false;
+            double carMilage = 0;
+            do
+            {
+                Console.Write("Enter Car #{i} Mileage: ");
+                input = Console.ReadLine();
+                IsCarMileageValid = double.TryParse(input, out carMilage);
+            }
+            while (IsCarMileageValid == false);
+
+            bool IsCarPriceValid = false;
+            double carPrice = 0;
+            do
+            {
+                Console.Write("Enter Car #{i} Price: ");
+                input = Console.ReadLine();
+                IsCarPriceValid = double.TryParse(input, out carPrice);
+            }
+            while (IsCarPriceValid == false);
+
+        }
+    }
+}
