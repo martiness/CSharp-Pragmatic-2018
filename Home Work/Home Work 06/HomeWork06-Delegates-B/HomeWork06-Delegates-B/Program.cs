b82521e854628ad4a3bbfc92be56a2e4b5f0568f
martiness <martin.kenov@gmail.com>
2018-11-19 18:14:14 +0200

    Home work 06 - Advanced

--- Home Work/Home Work 06/HomeWork06-Delegates-B/HomeWork06-Delegates-B/Program.cs
+++ Home Work/Home Work 06/HomeWork06-Delegates-B/HomeWork06-Delegates-B/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 //2. Delegates and Lambda Expressions
 
 //a.Декларирайте глобално в Program.cs делегат BooleanCondition, който връща bool и приема два аргумента a и b от тип int
@@ -23,43 +22,46 @@
 
 //a.Декларирайте глобално в Program.cs делегат BooleanCondition, който връща bool и приема два аргумента a и b от тип int
 
-namespace HomeWork06_Delegates
+namespace HomeWork06_Delegates_B
 {
-    //a.Декларирайте глобално в Program.cs делегат BooleanCondition, който връща bool и приема два аргумента a и b от тип int
-    public delegate bool BooleanCondition(int a, int b);
+    //e. (*) Направете BooleanCondition generic, т.е.да приема аргументи от произволен тип но винаги да връща bool. 
+    public delegate bool BooleanCondition<T>(T a, T b);
+
     public class Program
     {
         public static void Main(string[] args)
         {
             //b. В Main метода декларирайте 3 променливи от тип BooleanCondition (изберете подходящи имена спрямо (c))
-            BooleanCondition AGreaterThanB;
-            BooleanCondition ALessThanB;
-            BooleanCondition AEqualsToB;
+            BooleanCondition<int> greaterThan;
+            BooleanCondition<double> lessThan;
+            BooleanCondition<string> equalsTo;
 
             //c.Инициализирайте горните променливи с lambda expression-и, които:
-            Console.WriteLine("Lambda Examples - Home work 06 - A");
+            Console.WriteLine("Lambda Examples - Home work 06 - B");
             //i.Ако a > b връща true, иначе false
-            AGreaterThanB = (A, B) => A > B;       
+            greaterThan = (A, B) => A.CompareTo(B) > 0;
             //ii.Ако a<b връща true, иначе false
-            ALessThanB = (A, B) => A < B;
+            lessThan = (A, B) => A.CompareTo(B) < 0;
             //iii. Ако a = b връща true, иначе false
-            AEqualsToB = (A, B) => A == B;
+            equalsTo = (A, B) => A.CompareTo(B) == 0;
 
             //d. В Main метода използвайте горните 3 променливи (като методи!) за да изведете резултати на екрана при:
             //i.Ако a > b връща true, иначе false
             Console.Write("Lambda Example #1 (Check 7 > 77 ? True : False) => ");
-            Console.WriteLine(AGreaterThanB(7, 77));
+            Console.WriteLine(greaterThan(7, 77));
 
             //ii.a = 1 и b = 1
             Console.Write("Lambda Example #2 (Check 1 < 1 ? False : True) => ");
-            Console.WriteLine(ALessThanB(1, 1));
+            Console.WriteLine(lessThan(1.55, 2.33));
 
             //iii.a = 100 и b = 49
             Console.Write("Lambda Example #3 (Check 100 == 49 ? True : False) => ");
-            Console.WriteLine(AEqualsToB(100, 49));
-            
+            Console.WriteLine(equalsTo("100", "49"));
+
             Console.WriteLine("Press any key to quit ...");
             Console.ReadKey();
+
+
         }
     }
 }
